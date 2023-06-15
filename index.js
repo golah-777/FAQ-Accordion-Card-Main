3b24278512d5b2e468d8513637ddb98c932917bb
golah-777 <128902542+golah-777@users.noreply.github.com>
2023-06-15 17:47:59 +0200

    fixed some issues

--- index.js
+++ index.js
@@ -18,11 +18,14 @@ const answerFourEl = document.getElementById("answer-four");
 
 const answerFiveEl = document.getElementById("answer-five");
 
+const boldOneEL = document.getElementById("bold-one")
+const boldTwoEL = document.getElementById("bold-two")
+const boldThreeEL = document.getElementById("bold-three")
+const boldFourEL = document.getElementById("bold-four")
+const boldFiveEL = document.getElementById("bold-five")
+
 
 function answerNoOne (){
-
-  const boldOneEL = document.getElementById("bold-one")
-
   revealAnswerOneEl.style.transform = 'rotate(180deg)'
   boldOneEL.style.fontWeight = '800'
   boldOneEL.style.color = 'black'
@@ -30,53 +33,176 @@ function answerNoOne (){
 
   answerOneEl.style.display= 'inline'
   
-  
+  answerTwoEl.style.display= 'none'
+  answerThreeEl.style.display= 'none'
+  answerFourEl.style.display= 'none'
+  answerFiveEl.style.display= 'none'
+
+ // for other function to not work
+
+ revealAnswerFiveEl.style.transform = 'rotate(360deg)'
+ boldFiveEL.style.fontWeight = '400'
+ boldFiveEL.style.color = 'hsl(237, 12%, 33%)'
+ boldFiveEL.style.fontSize = '13px'
+
+ revealAnswerTwoEl.style.transform = 'rotate(360deg)'
+ boldTwoEL.style.fontWeight = '400'
+ boldTwoEL.style.color = 'hsl(237, 12%, 33%)'
+ boldTwoEL.style.fontSize = '13px'
+
+ revealAnswerThreeEl.style.transform = 'rotate(360deg)'
+ boldThreeEL.style.fontWeight = '400'
+ boldThreeEL.style.color = 'hsl(237, 12%, 33%)'
+ boldThreeEL.style.fontSize = '13px'
+
+ revealAnswerFourEl.style.transform = 'rotate(360deg)'
+ boldFourEL.style.fontWeight = '400'
+ boldFourEL.style.color = 'hsl(237, 12%, 33%)'
+ boldFourEL.style.fontSize = '13px'  
 };
 
 
 function answerNoTwo (){
- 
-  const boldTwoEL = document.getElementById("bold-two")
-
   revealAnswerTwoEl.style.transform = 'rotate(180deg)'
   boldTwoEL.style.fontWeight = '800'
   boldTwoEL.style.color = 'black'
   boldTwoEL.style.fontSize = '13px'
 
  answerTwoEl.style.display= 'inline'
+
+ answerOneEl.style.display= 'none'
+ answerThreeEl.style.display= 'none'
+ answerFourEl.style.display= 'none'
+ answerFiveEl.style.display= 'none'
+
+ // for other function to not work
+
+ revealAnswerOneEl.style.transform = 'rotate(360deg)'
+ boldOneEL.style.fontWeight = '400'
+ boldOneEL.style.color = 'hsl(237, 12%, 33%)'
+ boldOneEL.style.fontSize = '13px'
+
+ revealAnswerFiveEl.style.transform = 'rotate(360deg)'
+ boldFiveEL.style.fontWeight = '400'
+ boldFiveEL.style.color = 'hsl(237, 12%, 33%)'
+ boldFiveEL.style.fontSize = '13px'
+
+ revealAnswerThreeEl.style.transform = 'rotate(360deg)'
+ boldThreeEL.style.fontWeight = '400'
+ boldThreeEL.style.color = 'hsl(237, 12%, 33%)'
+ boldThreeEL.style.fontSize = '13px'
+
+ revealAnswerFourEl.style.transform = 'rotate(360deg)'
+ boldFourEL.style.fontWeight = '400'
+ boldFourEL.style.color = 'hsl(237, 12%, 33%)'
+ boldFourEL.style.fontSize = '13px' 
  
 };
 
 function answerNoThree (){
-  const boldThreeEL = document.getElementById("bold-three")
-
- 
   revealAnswerThreeEl.style.transform = 'rotate(180deg)'
   boldThreeEL.style.fontWeight = '800'
   boldThreeEL.style.color = 'black'
   boldThreeEL.style.fontSize = '13px'
 
   answerThreeEl.style.display= 'inline'
+
+  answerTwoEl.style.display= 'none'
+  answerOneEl.style.display= 'none'
+  answerFourEl.style.display= 'none'
+  answerFiveEl.style.display= 'none'
+
+  // for other function to not work
+
+  revealAnswerOneEl.style.transform = 'rotate(360deg)'
+  boldOneEL.style.fontWeight = '400'
+  boldOneEL.style.color = 'hsl(237, 12%, 33%)'
+  boldOneEL.style.fontSize = '13px'
+
+  revealAnswerTwoEl.style.transform = 'rotate(360deg)'
+  boldTwoEL.style.fontWeight = '400'
+  boldTwoEL.style.color = 'hsl(237, 12%, 33%)'
+  boldTwoEL.style.fontSize = '13px'
+
+  revealAnswerFiveEl.style.transform = 'rotate(360deg)'
+  boldFiveEL.style.fontWeight = '400'
+  boldFiveEL.style.color = 'hsl(237, 12%, 33%)'
+  boldFiveEL.style.fontSize = '13px'
+
+  revealAnswerFourEl.style.transform = 'rotate(360deg)'
+  boldFourEL.style.fontWeight = '400'
+  boldFourEL.style.color = 'hsl(237, 12%, 33%)'
+  boldFourEL.style.fontSize = '13px'
 };
 
 function answerNoFour (){
-  const boldFourEL = document.getElementById("bold-four")
-
   revealAnswerFourEl.style.transform = 'rotate(180deg)'
   boldFourEL.style.fontWeight = '800'
   boldFourEL.style.color = 'black'
   boldFourEL.style.fontSize = '13px'
 
   answerFourEl.style.display= 'inline'
+
+  answerTwoEl.style.display= 'none'
+  answerThreeEl.style.display= 'none'
+  answerOneEl.style.display= 'none'
+  answerFiveEl.style.display= 'none'
+
+  // for other function to not work
+
+  revealAnswerOneEl.style.transform = 'rotate(360deg)'
+  boldOneEL.style.fontWeight = '400'
+  boldOneEL.style.color = 'hsl(237, 12%, 33%)'
+  boldOneEL.style.fontSize = '13px'
+
+  revealAnswerTwoEl.style.transform = 'rotate(360deg)'
+  boldTwoEL.style.fontWeight = '400'
+  boldTwoEL.style.color = 'hsl(237, 12%, 33%)'
+  boldTwoEL.style.fontSize = '13px'
+
+  revealAnswerThreeEl.style.transform = 'rotate(360deg)'
+  boldThreeEL.style.fontWeight = '400'
+  boldThreeEL.style.color = 'hsl(237, 12%, 33%)'
+  boldThreeEL.style.fontSize = '13px'
+
+  revealAnswerFiveEl.style.transform = 'rotate(360deg)'
+  boldFiveEL.style.fontWeight = '400'
+  boldFiveEL.style.color = 'hsl(237, 12%, 33%)'
+  boldFiveEL.style.fontSize = '13px'
 };
 
 function answerNoFive (){
-  const boldFiveEL = document.getElementById("bold-five")
- 
   revealAnswerFiveEl.style.transform = 'rotate(180deg)'
   boldFiveEL.style.fontWeight = '800'
   boldFiveEL.style.color = 'black'
   boldFiveEL.style.fontSize = '13px'
 
   answerFiveEl.style.display= 'inline'
+
+  answerTwoEl.style.display= 'none'
+  answerThreeEl.style.display= 'none'
+  answerFourEl.style.display= 'none'
+  answerOneEl.style.display= 'none'
+
+  // for other function to not work
+
+  revealAnswerOneEl.style.transform = 'rotate(360deg)'
+  boldOneEL.style.fontWeight = '400'
+  boldOneEL.style.color = 'hsl(237, 12%, 33%)'
+  boldOneEL.style.fontSize = '13px'
+
+  revealAnswerTwoEl.style.transform = 'rotate(360deg)'
+  boldTwoEL.style.fontWeight = '400'
+  boldTwoEL.style.color = 'hsl(237, 12%, 33%)'
+  boldTwoEL.style.fontSize = '13px'
+
+  revealAnswerThreeEl.style.transform = 'rotate(360deg)'
+  boldThreeEL.style.fontWeight = '400'
+  boldThreeEL.style.color = 'hsl(237, 12%, 33%)'
+  boldThreeEL.style.fontSize = '13px'
+
+  revealAnswerFourEl.style.transform = 'rotate(360deg)'
+  boldFourEL.style.fontWeight = '400'
+  boldFourEL.style.color = 'hsl(237, 12%, 33%)'
+  boldFourEL.style.fontSize = '13px'
 };
